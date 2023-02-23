4399f24ed46dbf49965269e5fa15a98fafac19cc
dkornel01 <kornel.dobrocsi@gmail.com>
2023-02-23 16:38:54 +0100

    túlterheles masodik

--- src/java_oraimunka/Equalizer.java
+++ src/java_oraimunka/Equalizer.java
@@ -11,14 +11,15 @@ public class Equalizer {
 
     private static void equalizer5() {
         eq();
-        eq(12);
-        eq();
+        eq(12 ,true);
+        eq(true);
         eq(8);
         eq();
     }
     public static void eq(){
         int db=rnd.nextInt(3,8);
-        eq(db);
+        boolean igaz=true;
+        eq(db,true);
 
     }
 
@@ -30,5 +31,21 @@ public class Equalizer {
         }
         System.out.print("\n");
     }
-    
+    private static void eq(int a,boolean igaz) {
+        String szín="*";
+        for (int i=0;i<a;i++) {
+        System.out.print(szín+"");
+        
+        }
+        System.out.print("\n");
+    }
+    private static void eq(boolean igaz) {
+        int a=rnd.nextInt(3,8);
+        String szín="*";
+        for (int i=0;i<a;i++) {
+        System.out.print(szín+"");
+        
+        }
+        System.out.print("\n");
+    }
 }
