b6ff19374dbf362a6aa26f91b483aa0fc93e3c56
dkornel01 <kornel.dobrocsi@gmail.com>
2023-02-23 15:16:51 +0100

    main

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -7,14 +7,22 @@ public class JavaApplication4 {
 
     public static void main(String[] args) {
         fo();
+        osszead(1,2);
+        kiir();
     }
     
-    private static void fo(){
+    private static int fo(){
         int osszeg=0;
-        String[]tomb=new String[];
-        for (int=i;i>tomb.length();i++ ){
-        
+        for (int i=0;i<10;i++ ){
+            osszeg+=i;
         }
+        return osszeg;
+    }
+    private static int osszead(int a, int b){
+        return(a+b);
+    }
+    private static void kiir(String szoveg){
+        System.out.print(szoveg);
     }
     
 }
