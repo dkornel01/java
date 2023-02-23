5089fb42a33daf2d96d38d71d5e02d9a0c7b3636
dkornel01 <kornel.dobrocsi@gmail.com>
2023-02-23 15:34:08 +0100

    osszeadas kez

--- src/java_oraimunka/java_oraimunka.java
+++ src/java_oraimunka/java_oraimunka.java
@@ -8,11 +8,11 @@ public class java_oraimunka {
     public static void main(String[] args) {
         int osszeg=elso10Szamosszeg();
         String kimenet= "Az első 10 szám összege:" +osszeg+"\n"; 
-        osszead(1,2);
         kiir(kimenet);
         int szam1=7 ,szam2=4;
         osszeg= osszead(szam1, szam2);
         kiir("%d + %d=%d\n" .formatted(szam1,szam2,osszeg));
+        osszeadas(szam1,szam2,osszeg,elso10Szamosszeg());
     }
     
     private static int elso10Szamosszeg(){
@@ -28,6 +28,9 @@ public class java_oraimunka {
     private static void kiir(String szoveg){
         System.out.print(szoveg);
     }
-    private static int osszeadas
+    private static int osszeadas(int szam1,int szam2,int osszeg,int elso10){
+        szam1=szam1+szam2+osszeg+elso10;
+        return szam1;
+    }
     
 }
