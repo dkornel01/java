a85b36654a163e15d4bad3f5d28ab0b066b6fc35
dkornel01 <kornel.dobrocsi@gmail.com>
2023-02-23 15:07:32 +0100

    nincs kesz

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -6,7 +6,15 @@ public class JavaApplication4 {
 
 
     public static void main(String[] args) {
+        fo();
+    }
+    
+    private static void fo(){
+        int osszeg=0;
+        String[]tomb=new String[];
+        for (int=i;i>tomb.length();i++ ){
         
+        }
     }
     
 }
